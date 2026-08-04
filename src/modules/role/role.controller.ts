6a99926ccc425d34e61a40c7fcd4142fcca2f57b
tanwei <tanwei@jilinxiangyun.com>
2026-08-04 11:00:34 +0800

    feat: swagger文档示例

--- src/modules/role/role.controller.ts
+++ src/modules/role/role.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { RoleService } from './role.service';
-import { ApiTags, ApiOperation } from '@nestjs/swagger';
+import { ApiTags, ApiOperation, ApiQuery } from '@nestjs/swagger';
 @ApiTags('角色管理')
 @Controller('role')
 export class RoleController {
@@ -9,6 +9,18 @@ export class RoleController {
   @ApiOperation({
     summary: '获取角色列表', // 简短的标题，会直接显示在接口路径旁边
     description: '分页获取所有角色信息', // 详细描述，折叠在内部
+  })
+  @ApiQuery({
+    name: 'page',
+    required: true,
+    type: Number,
+    description: '页码，默认 1',
+  })
+  @ApiQuery({
+    name: 'limit',
+    required: true,
+    type: Number,
+    description: '每页条数，默认 10',
   })
   getList() {
     return this.roleService.getList();
--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
-// import { PinoLogger } from 'nestjs-pino';
+import { Controller, Get, Query } from '@nestjs/common';
+import { PinoLogger } from 'nestjs-pino';
 import { UserService } from './user.service';
-import { ApiTags, ApiOperation } from '@nestjs/swagger';
+import { ApiTags, ApiOperation, ApiQuery } from '@nestjs/swagger';
 @ApiTags('用户管理')
 @Controller('user')
 export class UserController {
   constructor(
-    // private readonly pinoLogger: PinoLogger,
+    private readonly pinoLogger: PinoLogger,
     private readonly userService: UserService,
   ) {}
   @Get('list')
@@ -14,8 +14,21 @@ export class UserController {
     summary: '获取用户列表', // 简短的标题，会直接显示在接口路径旁边
     description: '分页获取所有用户信息', // 详细描述，折叠在内部
   })
-  getList() {
-    // this.pinoLogger.info('user list 请求成功');
+  @ApiQuery({
+    name: 'page',
+    required: true,
+    type: Number,
+    description: '页码，默认 1',
+  })
+  @ApiQuery({
+    name: 'limit',
+    required: true,
+    type: Number,
+    description: '每页条数，默认 10',
+  })
+  // @ApiOkResponse({ description: '成功', type: RoleListResponseDto })
+  getList(@Query('page') page?: number, @Query('limit') limit?: number) {
+    this.pinoLogger.info(`${page}, ${limit} user list 参数`);
     return this.userService.getList();
   }
 }
